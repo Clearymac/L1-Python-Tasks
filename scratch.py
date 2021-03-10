b8ec1c8407e670f8e6fc4829a526d19dd9d584b0
Clearymac <clearyd@stu.otc.school.nz>
2021-03-11 09:38:10 +1300

    done?

--- scratch.py
+++ scratch.py
@@ -1,1 +1,16 @@
-print("jo mama")
+game_1 = 3
+game_2 = 6
+game_3 = 10
+game_4 = 10
+game_5 = 6
+
+print("""The games scored as follows: 
+Game 1: {}
+Game 2: {}
+Game 3: {}
+Game 4: {}
+Game 5: {}""".format(game_1, game_2, game_3, game_4, game_5))
+
+total_points = game_1 + game_2 + game_3 + game_4 + game_5
+
+print("the total points scored was {}".format(total_points))
